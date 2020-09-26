76cc0f62d13a170c1d4c4057a4bb2e3a5b60490f
Trevor Freeman <tefreeman@crimson.ua.edu>
2020-09-26 15:45:54 -0500

    f

--- mysocket.py
+++ mysocket.py
@@ -2,7 +2,7 @@
 import keyboard
 import socket
 import time
-HOST = '192.168.1.8'  # The server's hostname or IP address
+HOST = '192.168.1.7'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 
@@ -18,8 +18,8 @@ class MySocket:
     def connect(self):
         self.sock.connect((HOST, PORT))
 
-    def mysend(self, msg: str):
-        payload_bytes = str.encode(msg)
+    def mysend(self, eve: str, msg: str):
+        payload_bytes = str.encode(eve + '.' + msg)
         sent = self.sock.send(payload_bytes)
         if sent == 0:
             raise RuntimeError("socket connection broken")
@@ -32,21 +32,21 @@ TestSocket.connect()
 
 time.sleep(1)
 
-keyboard.add_hotkey('shift+q', TestSocket.mysend, args=('q'))
-keyboard.add_hotkey('shift+w', TestSocket.mysend, args=('w'))
-keyboard.add_hotkey('shift+e', TestSocket.mysend, args=('e'))
-keyboard.add_hotkey('shift+r', TestSocket.mysend, args=('r'))
+keyboard.add_hotkey('shift+q', TestSocket.mysend, args=['pr', 'q'])
+keyboard.add_hotkey('shift+w', TestSocket.mysend, args=['pr','w'])
+keyboard.add_hotkey('shift+e', TestSocket.mysend, args=['pr','e'])
+keyboard.add_hotkey('shift+r', TestSocket.mysend, args=['pr','r'])
 
-keyboard.add_hotkey('alt+q', TestSocket.mysend, args=('ctrl+q'))
-keyboard.add_hotkey('alt+w', TestSocket.mysend, args=('ctrl+w'))
-keyboard.add_hotkey('alt+e', TestSocket.mysend, args=('ctrl+e'))
-keyboard.add_hotkey('alt+r', TestSocket.mysend, args=('ctrl+r'))
+keyboard.add_hotkey('alt+q', TestSocket.mysend, args=['pr','ctrl+q'])
+keyboard.add_hotkey('alt+w', TestSocket.mysend, args=['pr','ctrl+w'])
+keyboard.add_hotkey('alt+e', TestSocket.mysend, args=['pr','ctrl+e'])
+keyboard.add_hotkey('alt+r', TestSocket.mysend, args=['pr','ctrl+r'])
 
-keyboard.add_hotkey('shift+f1', TestSocket.mysend, args=('f1'))
-keyboard.add_hotkey('shift+f2', TestSocket.mysend, args=('f2'))
-keyboard.add_hotkey('shift+f3', TestSocket.mysend, args=('f3'))
-keyboard.add_hotkey('shift+f4', TestSocket.mysend, args=('f4'))
-keyboard.add_hotkey('shift+f5', TestSocket.mysend, args=('f5'))
+keyboard.add_hotkey('shift+f1', TestSocket.mysend, args=['t','f1'])
+keyboard.add_hotkey('shift+f2', TestSocket.mysend, args=['t','f2'])
+keyboard.add_hotkey('shift+f3', TestSocket.mysend, args=['t','f3'])
+keyboard.add_hotkey('shift+f4', TestSocket.mysend, args=['t','f4'])
+keyboard.add_hotkey('shift+f5', TestSocket.mysend, args=['t','f5'])
 
 keyboard.wait()
     
--- server.py
+++ server.py
@@ -4,16 +4,41 @@ import time
 def pressAndRelease(key_str: str):
     keyboard.press_and_release(key_str)
 
+class KeyToggle:
+    def __init__(self):
+        self.keys = {}
+        
+    def add_key(self, key_str: str):
+        if not self.has_key(key_str):
+            self.keys[key_str] = False
+    
+    def trigger_key(self, key_str: str):
+        self.add_key(key_str)
+        
+        if self.keys[key_str] is True:
+            keyboard.release(key_str)
+        else: 
+            keyboard.press(key_str)
+                
+        self.keys[key_str] = not self.keys[key_str]
+            
+    def has_key(self, key_str: str):
+        return key_str in self.keys
+    
+    
+    
 
 import socket
 
-HOST = '192.168.1.8'  # Standard loopback interface address (localhost)
+HOST = '192.168.1.7'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
+    key_toggle = KeyToggle()
+    
     with conn:
         print('Connected by', addr)
         while True:
@@ -22,5 +47,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 break
             
             str_msg = data.decode("utf-8")
+            eve, msg = str_msg.split('.')
             
-            pressAndRelease(str_msg)
+            if eve == 'pr':
+                pressAndRelease(msg)
+            elif eve == 't':
+                key_toggle.trigger_key(msg)
